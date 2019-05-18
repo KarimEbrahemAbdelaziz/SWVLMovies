66a1a15d4ae7bdae85483e5b4188fb28c5e9c3b4
KarimEbrahemAbdelaziz <karimabdelazizmansour@gmail.com>
2019-05-19 01:49:40 +0200

    Get json object from resource through JSONLoader

---
+++ SWVLMovies/Utilities/Helpers/JSONLoader.swift
@@ -0,0 +1,38 @@
+//
+//  JSONLoader.swift
+//  SWVLMovies
+//
+//  Created by Karim Ebrahem on 5/19/19.
+//  Copyright © 2019 Karim Ebrahem. All rights reserved.
+//
+
+import Foundation
+
+class JSONLoader {
+    enum JSONLoaderError: Error {
+        case dataLoadingFailed
+        case jsonSerializationFailed
+        case castingJsonToDictionaryFailed
+        case pathNotFound
+    }
+    
+    static func loadJSON(from resourceName: String) -> Result<[String: Any], JSONLoaderError> {
+        if let path = Bundle.main.path(forResource: resourceName, ofType: "json") {
+            guard let data = try? Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe) else {
+                return .failure(.dataLoadingFailed)
+            }
+            
+            guard let jsonResult = try? JSONSerialization.jsonObject(with: data, options: .mutableLeaves) else {
+                return .failure(.jsonSerializationFailed)
+            }
+            
+            guard let jsonDictionary = jsonResult as? Dictionary<String, AnyObject> else {
+                return .failure(.castingJsonToDictionaryFailed)
+            }
+            
+            return .success(jsonDictionary)
+        } else {
+            return .failure(.pathNotFound)
+        }
+    }
+}
